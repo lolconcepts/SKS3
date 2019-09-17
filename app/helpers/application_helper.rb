8ab65571c529f4228113b94bb83a3de742809f19
Bryan Stewart <bstewart@lolconcepts.com>
2019-09-17 15:53:31 -0400

    changed getSK

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,7 @@ module ApplicationHelper
         def getSK()
     	      Socket.ip_address_list.each do |ip_addr|
                 if ip_addr.ip_address != "127.0.0.1"
-                  @sidekick = ip_addr.ip_address 
+                  @sidekick = current_url 
                 end
 	      end
 	      return @sidekick
