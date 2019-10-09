9739319f6ca692b72eff63921180dee87e64f3ae
Bryan Stewart <bstewart@lolconcepts.com>
2019-10-09 07:38:44 -0400

    created copy of tuitioncontroller for tuitionscontroller

---
+++ app/controllers/tuitions_controller.rb
@@ -0,0 +1,35 @@
+class TuitionsController < ApplicationController
+def index
+  respond_to do |format|
+      format.html # index.html.erb
+      format.mobile
+    end
+end
+
+def new
+  
+end
+
+def create
+  # Amount in cents
+  @amount = 500
+
+  customer = Stripe::Customer.create({
+    email: params[:stripeEmail],
+    source: params[:stripeToken],
+  })
+
+  charge = Stripe::Charge.create({
+    customer: customer.id,
+    amount: @amount,
+    description: 'Rails Stripe customer',
+    currency: 'usd',
+  })
+  redirect_to root_path
+rescue Stripe::CardError => e
+  flash[:error] = e.message
+  redirect_to root_path
+end
+
+
+end
