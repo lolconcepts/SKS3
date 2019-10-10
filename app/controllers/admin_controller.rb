f07dd83d1033b6b902a57f08929a968df18cd441
Bryan Stewart <bstewart@lolconcepts.com>
2019-10-10 14:08:05 -0400

    changed index html and added student count to admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,7 @@
 class AdminController < ApplicationController
 before_action :authenticate_user!
 def index
+	@student_count = Student.count
 	@dojo = Dojo.first
 	@Version = "19.10" # YEAR.MONTH of update
 end
