64e5faf638ed583371c3222f3e211ba65a10e9be
Bryan Stewart <bstewart@lolconcepts.com>
2019-10-10 08:47:04 -0400

    changed css, added background

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
 before_action :authenticate_user!
 def index
 	@dojo = Dojo.first
-	@Version = 19.10 # YEAR.MONTH of update
+	@Version = "19.10" # YEAR.MONTH of update
 end
 end
