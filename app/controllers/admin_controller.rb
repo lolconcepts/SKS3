5f7662b26540dedcda26d0fa2549af29d815c7b9
Bryan Stewart <bstewart@lolconcepts.com>
2019-10-11 10:38:32 -0400

    added students var to admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
 before_action :authenticate_user!
 def index
 	@student_count = Student.count
+    @students = Student.all
 	@dojo = Dojo.first
 	@Version = "19.10" # YEAR.MONTH of update
 	# Get Attendance Info For Graph
