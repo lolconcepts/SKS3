d38fd806e2e9e581ab14cc2131e4dc2e4b80036b
Bryan Stewart <bstewart@lolconcepts.com>
2019-10-18 15:22:48 -0400

    fixed helper for dates

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,7 +1,11 @@
 module StudentsHelper
 	def daysAtRank(student_id)
 		@today = Time.now().to_date
-		@last_promo = Student.find(student_id).last_promotion.to_date
+		if Student.find(student_id).last_promotion != nil
+			@last_promo = Student.find(student_id).last_promotion.to_date
+		else
+			@last_promo = @today
+		end
 		@days = (@today - @last_promo).to_i
 		return @days
 	end
